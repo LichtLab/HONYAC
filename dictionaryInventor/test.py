6e39bfd5b730294c5c960e2d315352090cb8a384
ShoSekine <sekine_sho@yahoo.co.jp>
2015-06-30 20:47:01 +0900

    単語抜出ツール

--- dictionaryInventor/test.py
+++ dictionaryInventor/test.py
@@ -2,17 +2,18 @@
 import urllib
 import chardet
 from pyquery import PyQuery as pq
+import string
+import re
 
-# テスト用URL
-# METAタグでcharset指定してなくてもちゃんと文字コード判別できるかもテスト
+# 抜き出すURL
 urls = [
-    'http://www.yahoo.co.jp/',               # UTF-8
+    'https://esportes.yahoo.com/noticias/guerrero-peru-luta-mas-chile-vence-decide-copa-012402749--spt.html',               # UTF-8
     'https://br.yahoo.com/',              # EUC-JP
     ]
 
 
-if __name__ == '__main__':
-    # 各サイトの文字コードを判別
+def extractwords():
+	 # 各サイトの文字コードを判別
     detected = []
     for url in urls:
         data = ''.join(urllib.urlopen(url).readlines())
@@ -20,15 +21,26 @@ if __name__ == '__main__':
         result = dict(url=url,data=data,**guess)
         detected.append(result)
 
-    # 各サイトごとにpタグの文字とリンク先を引っ張ってくる
+    # 各サイトごとに特定タグの文字とリンク先を引っ張ってくる
     for p in detected:
         print '%s -> %s (%s)' % (p['url'], p['encoding'], p['confidence'])
         unicoded = p['data'].decode(p['encoding'])  # デコード
         d = pq(unicoded)
-        for link in d.find('p'):  # 見つけたaタグごとにタイトルとURLを抜き出す
+        for link in d.find('p'):  # pタグで抜出し
             link_title = pq(link).text()
-            if link_title != '\r\n':
-	            try:
-	            	print link_title
-	            except:
-	            	print 'err'
+            #スペースで区切る（単語区切り）
+            words = link_title.split();
+            for word in words:
+            	word.rstrip('\W')
+            	#英数字以外を削除
+            	word = re.sub(r'[\W]+', "", word)#英数字以外の文字を削除
+            	word = re.sub(r'[0-9,_]+', "", word)#数字、アンダースコアを削除
+            	word = word.lower()#小文字に変換
+            	if word != '\r\n' and word != '' and len(word) >= 2:#1文字のものは削除
+            		try:
+            			print word
+            		except:
+            			pass
+
+if __name__ == '__main__':
+   extractwords()
