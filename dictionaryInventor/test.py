7df05bc78c99a974e74a31e4cbce9b47e420ba34
ShoSekine <sekine_sho@yahoo.co.jp>
2015-07-02 08:16:30 +0900

    update

--- dictionaryInventor/test.py
+++ dictionaryInventor/test.py
@@ -7,9 +7,9 @@ import re
 import urllib2
 import lxml.html
 
-URL_ABSOLUTE = "http://www.correiobraziliense.com.br"
+MAX_URL_COUNTS = 500
 
-def extractwords(urls):
+def extractwords_fromwebpagelist(urls):
 	# 各サイトの文字コードを判別
     returnurls = []
     detected = []
@@ -28,16 +28,79 @@ def extractwords(urls):
             #スペースで区切る（単語区切り）
             words = link_title.split();
             for word in words:
-            	word.rstrip('\W')
-            	#英数字以外を削除
-            	word = re.sub(r'[\W]+', "", word)#英数字以外の文字を削除
-            	word = re.sub(r'[0-9,_]+', "", word)#数字、アンダースコアを削除
-            	word = word.lower()#小文字に変換
-            	if word != '\r\n' and word != '' and len(word) >= 2:#1文字のものは削除
-            		try:
-            			print word
-            		except:
-            			pass
+                word.rstrip('\W')
+                #英数字以外を削除
+                word = re.sub(r'[\W]+', "", word)#英数字以外の文字を削除
+                word = re.sub(r'[0-9,_]+', "", word)#数字、アンダースコアを削除
+                word = word.lower()#小文字に変換
+                if word != '\r\n' and word != '' and len(word) >= 2:#1文字のものは削除
+                    try:
+                        print word
+                    except Exception, e:
+                        print 'exeption0'
+                        print e.value
+                        pass
+
+
+
+'''
+入力　list(dic)...dic = {'base' : 'http;//www.sekine.com/index', 'absolute' : 'http;//www.sekine.com'}
+出力　list(url)...url = 'http;//www.sekine.com/index'
+入力にあるページにあるリンクを抽出してurl文字列のリストとして返す。相対パスは絶対パスに変換される
+'''
+def getlinkurllist(url_diclist):
+    newurls = urls
+    #登録したスタート地点のリンクでループ
+    for targeturl in urls:
+        try:
+            html = urllib2.urlopen(targeturl['base']).read()
+            dom = lxml.html.fromstring(html)
+            dom.make_links_absolute(targeturl['absolute'])
+            links_from_targeturl = dom.xpath("//@href")
+            print 'begin url parsing'
+            print targeturl['base']
+            #リンク先の全てのurlをチェック
+            for urlele in links_from_targeturl:
+                if urlele.find('http') or urlele.find('https'):
+                    #特定のリンク先を落とすフィルタリング
+                    if not urlele.find('css') or not urlele.find('jpg') or not urlele.find('png') or not urlele.find('js') or urlele.find('javascript'): 
+                        if urlele.find('mail'):
+                            try:
+                                #urleleが既に登録済みのリンクでないかをチェック
+                                existingflag = False
+                                for targeturl_sub in newurls:
+                                    if targeturl_sub['base'] == urlele:
+                                        existingflag = True
+                                if existingflag == False:
+                                    #print urlele
+                                    newurl_dic = {'base': urlele, 'absolute': targeturl['absolute']}
+                                    newurls.append(newurl_dic)
+                                    print len(newurls)
+                                    if len(newurls) > MAX_URL_COUNTS:
+                                        raise Exception ('Interaption')
+                            except Exception, e:
+                                if len(newurls) > MAX_URL_COUNTS:
+                                    print 'exception1'
+                                    raise Exception ('Interaption')
+                                else:
+                                    print 'exception1_sub'
+                                    print len(newurls)
+                                    pass
+        except Exception, e:
+            if len(newurls) > MAX_URL_COUNTS:
+                print 'exception2'
+                break
+            else:
+                print 'exception2_sub'
+                print len(newurls)
+                pass
+
+    #集めた新しいurlリンク集まりで単語パースするurlのリストを作る
+    wordextraction_targeturls = []
+    for url in newurls:
+        wordextraction_targeturls.append(url['base'])
+    return wordextraction_targeturls
+
 
 if __name__ == '__main__':
     urls = [
@@ -48,23 +111,11 @@ if __name__ == '__main__':
     {'base' : 'http://oglobo.globo.com/'                ,'absolute' : 'http://oglobo.globo.com'},
     {'base' : 'http://www.zerohora.com'                 ,'absolute' : 'http://www.zerohora.com'},
         ]
-    for url in urls:
-        try:
-            html = urllib2.urlopen(url['base']).read()
-            dom = lxml.html.fromstring(html)
-            dom.make_links_absolute(URL_ABSOLUTE)
-            urlList = dom.xpath("//@href")
-            for urlele in urlList:
-                if urlele.find('http') or urlele.find('https'):
-                    if not urlele.find('css') or urlele.find('jpg') or urlele.find('png') or urlele.find('js') or urlele.find('javascript'): 
-                        try:
-                            urldic = {'base': urlele, 'absolute': url['absolute']}
-                            print urldic                
-                        except Exception, e:
-                            pass
-        except Exception, e:
-            pass
 
-    #urls = extractwords(urls)
-    #urls = extractwords(urlList)
-    #print urls
+    targeturls = []
+    # targeturls = getlinkurllist(urls)
+    print 'EXTRACTING BEGIN'
+    testurl = [
+    'http://www1.folha.uol.com.br/poder/2015/06/1650009-em-derrota-do-governo-senado-aprova-reajuste-para-servidores-do-judiciario.shtml',
+    ]
+    extractwords_fromwebpagelist(testurl)
