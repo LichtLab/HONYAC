2d122fe90efaea4616a45837b1adcca6dcf20d48
ShoSekine <sekine@server.sscreate.private>
2015-08-05 07:43:44 +0900

    update chinese dic

--- dictionaryInventor/indexcreator.py
+++ dictionaryInventor/indexcreator.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 
-con = sqlite3.connect("wordBank_spa_jpn.db")
+con = sqlite3.connect("wordBank_en_ch.db")
 cur = con.cursor()
 cur.execute("CREATE INDEX fromwordindex ON wordTable(from_word)")
 con.commit()
